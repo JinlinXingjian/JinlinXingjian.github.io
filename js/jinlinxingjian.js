22367d7448582f18138b763d6c848a0cf6a20944
JinlinXingjian <2647600900@qq.com>
2023-05-16 13:43:22 +0800

    Site updated: 2023-05-16 13:43:18

---
+++ js/jinlinxingjian.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    if (window.location.href == 'http://localhost:4000/' || window.location.href == 'https://jinlinxingjian.top/' || window.location.href == 'https://www.jinlinxingjian.top/') 
+    {
+        Snackbar.show({
+            actionText: '关闭',
+            text: '欢迎来到 金鳞星溅 的小站',
+            backgroundColor: '#e27474',
+            actionTextColor: '#ffffff',
+            pos: 'bottom-center',
+            duration: '2000',
+            width: '100vw'
+        });
+        document.querySelector("body").classList.add("indexBgImg")
+        window.addEventListener('scroll', function() {
+            let scrollTop = window.scrollY;
+            let windowHeight = window.innerHeight; 
+            let scrollPercent = (1.0-(scrollTop /windowHeight)).toFixed(1);
+            scrollPercent=Number(scrollPercent);
+            if(scrollPercent < Number(0.6))
+            {
+                scrollPercent=Number(0.6);
+            }
+            document.querySelector("#page-header").style.filter="brightness("+scrollPercent+")";
+            document.querySelector("#content-inner").style.backdropFilter="brightness("+scrollPercent+")";
+        })
+    }
+}  
