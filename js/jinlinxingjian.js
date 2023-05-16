ba7b8a92ffc962476ddd9d8fd893335785ac13f2
JinlinXingjian <2647600900@qq.com>
2023-05-16 14:07:37 +0800

    Site updated: 2023-05-16 14:07:37

--- js/jinlinxingjian.js
+++ js/jinlinxingjian.js
@@ -1,4 +1,22 @@
 window.onload = function () {
+    
+    fetch('https://api64.ipify.org?format=json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var ipAddress = data.ip;
+  
+      // 在这里可以使用获取到的IP地址进行进一步的处理
+      console.log('访问者的IP地址：', ipAddress);
+    })
+    .catch(function(error) {
+      console.log('获取访问者IP地址失败', error);
+    });
+  
+
+
+
     if (window.location.href == 'http://localhost:4000/' || window.location.href == 'https://jinlinxingjian.top/' || window.location.href == 'https://www.jinlinxingjian.top/') 
     {
         Snackbar.show({
