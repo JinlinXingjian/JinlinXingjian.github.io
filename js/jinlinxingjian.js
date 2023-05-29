38ec0ce5d5bc9b0d1a9ed551afc68c0d39a37801
JinlinXingjian <2647600900@qq.com>
2023-05-29 13:17:19 +0800

    Site updated: 2023-05-29 13:17:18

--- js/jinlinxingjian.js
+++ js/jinlinxingjian.js
@@ -3,47 +3,47 @@ window.onload = function () {
     {
       
       //获取ip呢
-        // fetch('http://ip-api.com/json/')
-        // .then(function(response) {
-        //   return response.json();
-        // })
-        // .then(function(data) {
-        //   var city = data.city;
-        //   var country = data.country_name;
-        //   let line
+        fetch('http://ip-api.com/json/')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          var city = data.city;
+          var country = data.country_name;
+          let line
 
 
-        //   if(country=="China"){
-        //     line="腾讯云线路"
-        //   }
-        //   else{
-        //     line="GIthub pages线路"
-        //   }
-        //   let text='欢迎来到 金鳞星溅 的小站,你访问的线路是'+line
-        //   Snackbar.show({
-        //       actionText: '关闭',
-        //       text: text,
-        //       backgroundColor: '#e27474',
-        //       actionTextColor: '#ffffff',
-        //       pos: 'bottom-center',
-        //       duration: '2000',
-        //       width: '60vw'
-        //   });
-        //   // 找到目标元素
-        //   var footerWrap = document.getElementById('footer-wrap');
-        //   var footerCustomText = footerWrap.querySelector('.footer_custom_text');
+          if(country=="China"){
+            line="腾讯云线路"
+          }
+          else{
+            line="GIthub pages线路"
+          }
+          let text='欢迎来到 金鳞星溅 的小站,你访问的线路是'+line
+          Snackbar.show({
+              actionText: '关闭',
+              text: text,
+              backgroundColor: '#e27474',
+              actionTextColor: '#ffffff',
+              pos: 'bottom-center',
+              duration: '2000',
+              width: '60vw'
+          });
+          // 找到目标元素
+          var footerWrap = document.getElementById('footer-wrap');
+          var footerCustomText = footerWrap.querySelector('.footer_custom_text');
 
-        //   // 创建 <div> 元素
-        //   var divElement = document.createElement('div');
-        //   divElement.textContent = '当前访问线路是'+line;
+          // 创建 <div> 元素
+          var divElement = document.createElement('div');
+          divElement.textContent = '当前访问线路是'+line;
 
-        //   // 将 <div> 元素插入到目标元素中
-        //   footerCustomText.appendChild(divElement);
+          // 将 <div> 元素插入到目标元素中
+          footerCustomText.appendChild(divElement);
 
-        // })
-        // .catch(function(error) {
-        //   console.log('获取访问者地理位置信息失败', error);
-        // });
+        })
+        .catch(function(error) {
+          console.log('获取访问者地理位置信息失败', error);
+        });
         //site-name修改
         document.querySelector('.site-name').textContent='金鳞星溅'
         //滚动变灰
@@ -60,6 +60,8 @@ window.onload = function () {
             document.querySelector("#page-header").style.filter="brightness("+scrollPercent+")";
             document.querySelector("#content-inner").style.backdropFilter="brightness("+scrollPercent+")";
         })
+        //加载 嘉然
+        加载圣·嘉然();
     }
     
 }  
